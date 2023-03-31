321d1dbe818cb2478ca814330771de3ba4d7fa5b
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-03-31 17:03:43 +0700

    enambbahkan void input

--- Quickshort_064/Quickshort_064.cpp
+++ Quickshort_064/Quickshort_064.cpp
@@ -7,3 +7,25 @@ int cmp_count = 0; // number of comprasion
 int mov_count = 0; // number of data movemnts
 int n;
 
+void input() {
+	while (true)
+	{
+		cout << "Masukkan panajng element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+
+	cout << "\n------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
