05d5d32179308ba89f2d25fdeab4c9de7a48a9d8
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-03-31 21:12:11 +0700

    tambahkan while

--- Quickshort_064/Quickshort_064.cpp
+++ Quickshort_064/Quickshort_064.cpp
@@ -53,4 +53,27 @@ void q_short(int low, int high)
 	j = high;						//langkah 4
 
 	while (i <= j)					//langkah 10
-}
+	{ 
+		//search for an element greater than pivot 
+		while ((arr[i] <= pivot) && (i <= high))	//langkah 5
+		{
+			i++;									//langkah 6
+			cmp_count++;
+		}
+		cmp_count++;
+		//search for an element less than or equal to pivot 
+		while ((arr[j] > pivot) && (j >= low))		//langkah 7
+		{
+			j--;									//langkah 8
+			cmp_count++;
+		}
+		cmp_count++;
+
+		//Langkah 9
+		if (i < j) // if the greater element is on the left of the element
+		{
+			//swap the element at index i whit the element at index j
+			swap(i, j);
+			mov_count++;
+		}
+	}
